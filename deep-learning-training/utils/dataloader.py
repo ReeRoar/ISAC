5ddc830180ce4afaba14c0eab29e32c594652a78
zachary <zbiernat2000@gmail.com>
2023-01-21 21:11:59 -0500

    add load_coco_dataset helper function

---
+++ deep-learning-training/utils/dataloader.py
@@ -0,0 +1,21 @@
+import fiftyone.zoo as foz
+
+
+def load_coco_dataset(split: str, overwrite: bool = False):
+    """
+    Loads in microsoft COCO dataset from fiftyone
+    :param split: Split of data. Must be train or validation
+    :param overwrite: boolean value representing whether to overwrite existing files
+    :return: loaded in dataset
+    """
+    try:
+        data = foz.load_zoo_dataset(
+            "coco-2017",
+            split=split,
+            classes=["person", ],
+            only_matching=True,
+            overwrite=overwrite
+        )
+        return data
+    except:
+        return load_coco_dataset(split, overwrite)
