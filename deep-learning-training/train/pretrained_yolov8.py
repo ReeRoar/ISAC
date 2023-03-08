1e3f3db14ca3e94a58ff5fa4b535b922ae9576e3
zachary <zbiernat2000@gmail.com>
2023-03-08 17:44:08 -0500

    commiting to checkout

--- deep-learning-training/train/pretrained_yolov8.py
+++ deep-learning-training/train/pretrained_yolov8.py
@@ -1,4 +1,4 @@
 from ultralytics import YOLO
 
-model = YOLO('yolov8n.pt')
-model.train(data="/home/zach/fiftyone/coco-2017/person_coco.yaml", epochs=100, imgsz=640,patience=2)
+model = YOLO('runs/detect/train2/weights/last.pt')
+model.train(data="/home/zach/fiftyone/coco-2017/person_coco.yaml", epochs=100, imgsz=640,patience=5, optimizer='AdamW', lr0=0.0001,lrf=0.0001,cos_lr=True)
