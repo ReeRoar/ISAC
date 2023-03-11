565c70c646ce956dac9a17ef446479d191263df8
zachary <zbiernat2000@gmail.com>
2023-03-11 13:45:07 -0500

    add generic request class

---
+++ flask_backend/GenericRequest.py
@@ -0,0 +1,88 @@
+from flask import jsonify, abort
+from markupsafe import escape
+from marshmallow import (
+    ValidationError,
+)
+
+
+class GenericRequest:
+    def __init__(self, model, schema, db, has_reqparse=False):
+        """
+        Class in order to make model agnostic requests. This class only contains basic requests and should be extended
+        in order to create requests having joined tables.
+
+        :param model: Class in which the requests will be made from
+        :param schema: Model Schema
+        :param db: database session
+        :param has_reqparse: Boolean to represent whether or not this class has a reqparse
+        """
+        self.schema = schema
+        self.model = model
+        if has_reqparse:
+            self.parser = schema.get_parser()
+        self.db = db
+
+    def get_all(self):
+        """
+        Gets all rows of model
+        :return: list of JSON objects containing each row of the model within the database
+        """
+        query_result = self.model.query.all()
+        return jsonify([self.schema.dump(obj) for obj in query_result])
+
+    def get_by_id(self, id):
+        """
+        Gets singular object by id
+        :param id: primary key of object
+        :return: Object in which the id belongs to
+        """
+        model_id = escape(id)
+        model = self.model.query.get_or_404(model_id)
+        return jsonify(self.schema.dump(model))
+
+    def put_request(self, id):
+        """
+        Preforms a put request for given ID
+        :param id: value of primary key
+        :return: Success status of request
+        """
+        try:
+            model_id = escape(id)
+            model = self.model.query.get_or_404(model_id)
+            args = self.parser.parse_args()
+            for key, value in args.items():
+                if args[key] is not None:
+                    setattr(model, key, value)
+            self.db.session.commit()
+            return jsonify(success=True)
+        except ValidationError as err:
+            return {"errors": err.messages}, 422
+
+    def post_request(self, request):
+        """
+        preforms a post request to add object to database
+        :param request: the HTTP request containing model information
+        :return: Success status of request
+        """
+        json_input = request.json
+        try:
+            data = self.schema.load(json_input)
+            self.db.session.add(data)
+            self.db.session.commit()
+            return jsonify(success=True)
+        except ValidationError as err:
+            return {"errors": err.messages}, 422
+
+    def delete_request(self, id):
+        """
+        Preforms a delete request of the object with the primary key value of id
+        :param id: id of object being deleted
+        :return: Success status of request
+        """
+        model_id = escape(id)
+        x = self.model.query.filter_by(id=model_id).delete()
+        if x == 0:
+            abort(404)
+        self.db.session.commit()
+        resp = jsonify(success=True)
+        return resp
--- flask_backend/hello.py
+++ flask_backend/hello.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify, abort
 from markupsafe import escape
 from flask_sqlalchemy import SQLAlchemy
 import json
+from GenericRequest import GenericRequest
 from flask_marshmallow import Marshmallow
 from flask_restful import reqparse
 from marshmallow import (
@@ -18,7 +19,6 @@ db = SQLAlchemy()
 
 app = Flask(__name__)
 ma = Marshmallow(app)
-
 
 app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql://root:password@localhost/flask_test'
 
@@ -39,64 +39,45 @@ class Model(db.Model):
 class ModelSchema(ma.SQLAlchemySchema):
     class Meta:
         model = Model
+
     id = ma.auto_field()
     username = ma.auto_field()
     email = ma.auto_field()
+
     @post_load
-    def make_model(self,data,**kwargs):
+    def make_model(self, data, **kwargs):
         return Model(**data)
+
+    def get_parser(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('username', type=str, location='json')
+        parser.add_argument('email', type=str, location='json')
+        parser.add_argument('id', type=int, location='json')
+        return parser
+
 
 with app.app_context():
     db.create_all()
 
+model_generic = GenericRequest(Model, ModelSchema(), db, True)
+
 
 @app.route('/models', methods=['GET', 'POST'])
 def models():
-    model_schema = ModelSchema()
     if request.method == 'GET':
-        query_result = Model.query.all()
-        return jsonify([model_schema.dump(obj) for obj in query_result])
+        return model_generic.get_all()
     if request.method == 'POST':
-        json_input = request.json
-        try:
-            data = model_schema.load(json_input)
-            db.session.add(data)
-            db.session.commit()
-            return jsonify(success=True)
-        except ValidationError as err:
-            return {"errors": err.messages}, 422
+        return model_generic.post_request(request)
 
 
-@app.route('/models/<model_id>', methods=['GET', 'DELETE','PUT'])
+@app.route('/models/<model_id>', methods=['GET', 'DELETE', 'PUT'])
 def models_id(model_id):
-    model_schema = ModelSchema()
     if request.method == 'GET':
-        model_id = escape(model_id)
-        model = Model.query.get_or_404(model_id)
-        return jsonify(model_schema.dump(model))
+        return model_generic.get_by_id(model_id)
     if request.method == 'PUT':
-        json_input = request.json
-        try:
-            parser = reqparse.RequestParser()
-            parser.add_argument('username', type=str, location='json')
-            parser.add_argument('email', type=str, location='json')
-            parser.add_argument('id', type=int, location='json')
-            model = Model.query.get_or_404(model_id)
-            args = parser.parse_args()
-            for key, value in args.items():
-                if args[key] is not None:
-                    setattr(model, key, value)
-            db.session.commit()
-            return jsonify(success=True)
-        except ValidationError as err:
-            return {"errors": err.messages}, 422
+        return model_generic.put_request(model_id)
     if request.method == 'DELETE':
-        x = Model.query.filter_by(id=model_id).delete()
-        if x == 0:
-            abort(404)
-        db.session.commit()
-        resp = jsonify(success=True)
-        return resp
+        return model_generic.delete_request(model_id)
 
 
 if __name__ == '__main__':
