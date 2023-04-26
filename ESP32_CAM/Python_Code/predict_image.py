2ba37f2678eccea4c19eac4b918711c38afa4b12
zachary <zbiernat2000@gmail.com>
2023-04-25 21:58:54 -0400

    add predict image

---
+++ ESP32_CAM/Python_Code/predict_image.py
@@ -0,0 +1,28 @@
+from ultralytics import YOLO
+import cv2
+
+model = YOLO('best.pt')
+
+
+def pred_img(image, model=model, conf=.11):
+    x = model.predict(image, conf=conf, verbose=False)
+    return len(x[0].boxes.boxes)
+
+
+def crop_img(img,crop=(None,None,None,None)):
+    img = cv2.imread(img)
+    cropped_image = img[crop[0]:crop[1], crop[2]:crop[3]]
+    return cropped_image
+
+
+def two_image_result(image1,image2, crop1=(None,None,None,None), crop2=(None,None,None,None)):
+    cropped_img1 = crop_img(image1,crop1)
+    results1 = pred_img(cropped_img1)
+    cropped_img2 = crop_img(image2,crop2)
+    results2 = pred_img(cropped_img2)
+    return results1+results2
+
+
+
+
+#print(pred_img(crop_img('frame2494_2.jpg',(150,None,150,None)))) #150, 150 default for d316 camera 3
