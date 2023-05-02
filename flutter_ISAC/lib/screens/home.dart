87ce6e50cbb76d644d1c6a53ac61b0f3730f9157
Mac <mac@Macs-MacBook-Pro.local>
2023-05-01 21:45:54 -0400

    debug commit

--- flutter_ISAC/lib/screens/home.dart
+++ flutter_ISAC/lib/screens/home.dart
@@ -1,14 +1,15 @@
-// ignore_for_file: library_private_types_in_public_api
-
-import 'dart:convert';
-import 'package:url_launcher/url_launcher.dart';
+import 'dart:async';
+// ignore: avoid_web_libraries_in_flutter
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+// ignore: unused_import
 import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
+  // ignore: library_private_types_in_public_api
   _HomePageState createState() => _HomePageState();
 }
 
@@ -22,28 +23,21 @@ class _HomePageState extends State<HomePage> {
   }
 
   Future<void> loadData() async {
-    final response =
-        await http.get(Uri.parse('http://127.0.0.1:5000/attendance_student'));
+    try {
+      final dio = Dio();
+      final response =
+          await dio.get('http://127.0.0.1:5000/attendance_student');
 
-    if (response.statusCode == 200) {
-      final jsonList = jsonDecode(response.body) as List<dynamic>;
-      setState(() {
-        data = jsonList.cast<Map<String, dynamic>>().toList();
-      });
-    } else {
+      if (response.statusCode == 200) {
+        final jsonList = response.data as List<dynamic>;
+        setState(() {
+          data = jsonList.cast<Map<String, dynamic>>().toList();
+        });
+      } else {
+        throw Exception('Failed to load data from API');
+      }
+    } catch (error) {
       throw Exception('Failed to load data from API');
-    }
-  }
-
-  Future<void> sendEmail(String recipientEmail) async {
-    final String emailUrl =
-        'mailto:$recipientEmail?subject=Attendance Update&body=Hello, \n\n';
-    // ignore: deprecated_member_use
-    if (await canLaunch(emailUrl)) {
-      // ignore: deprecated_member_use
-      await launch(emailUrl);
-    } else {
-      throw 'Could not launch $emailUrl';
     }
   }
 
@@ -51,7 +45,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Class Attendance'),
+        title: const Text('Home'),
       ),
       body: ListView.builder(
         itemBuilder: (BuildContext context, int index) {
@@ -60,11 +54,7 @@ class _HomePageState extends State<HomePage> {
           return ListTile(
             title: Text('${student['first_name']} ${student['last_name']}'),
             subtitle: Text('Status: ${attendance['status']}'),
-            trailing: IconButton(
-              icon: const Icon(Icons.email),
-              onPressed: () => sendEmail(student['email']),
-            ),
-            //trailing: Text(student['email']),
+            trailing: Text(student['email']),
           );
         },
         itemCount: data.length,
